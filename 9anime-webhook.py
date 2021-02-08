698faa24b798f3130c3a2b7d4bedc072ed71070f
Kenny <nggkenny@gmail.com>
2021-02-08 14:02:01 +0530

    Initial Commit

--- 9anime-webhook.py
+++ 9anime-webhook.py
@@ -1,1 +1,11 @@
-print("Test")
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+webhook = DiscordWebhook(url='https://discord.com/api/webhooks/808249420750520351/7S3GqGkalYuzmNi8M9x6dU3KGjeR40sTbVv0d4ROSwtO_HbrjpBItAuiKfAtCMHtoEuI')
+
+embed = DiscordEmbed(title='Timeout Issued',
+                    description='Test description',
+                    color=242424)
+
+webhook.add_embed(embed)
+
+response = webhook.execute()
