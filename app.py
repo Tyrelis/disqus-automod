cad7b588cb47dca6cdaec1b5830f561e68d19473
Kenny <nggkenny@gmail.com>
2021-02-09 17:23:10 +0530

    Commit

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
 from discord_webhook import webhook
 from flask import *
 import pyrebase
-#from .discordhook import DiscordAlert
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import requests
+import json
+
+webhook = DiscordWebhook(url='https://discord.com/api/webhooks/808249420750520351/7S3GqGkalYuzmNi8M9x6dU3KGjeR40sTbVv0d4ROSwtO_HbrjpBItAuiKfAtCMHtoEuI')
+
+API_KEY = 'nl55taKAfLJah7i8EaglOrFEoifCONO9SwOno5DiNanv0FPXwlxufjQ7DIP3drhc'
+
+class DiscordAlert:
+    def __init__(self, comment_id):
+
+        global API_KEY
+
+        url = 'https://disqus.com/api/3.0/posts/details.json?api_key={}&post={}'.format(API_KEY, comment_id)
+        
+        response = requests.get(url)
+        response = json.loads(response.text)
+        
+        self.user = response['response']['author']['username']
+        self.comment_id = comment_id
+        self.timeout_duration = "1 Days"
+        self.mod = "Kenny Senpai"
+        self.comment_url = 'https://9anime-to.disqus.com/admin/moderate/pending/search/id:{}'.format(comment_id)
+
+    def send_alert(self):
+        embed = DiscordEmbed(title='Timeout Issued',
+                            color=0x5A2E98)
+
+        embed.set_author(name="View Comment", url=self.comment_url)
+
+        embed.add_embed_field(name="User", value=self.user)
+        embed.add_embed_field(name="Comment ID", value=self.comment_id)
+        embed.add_embed_field(name="Timeout Duration", value=self.timeout_duration)
+        embed.add_embed_field(name="Moderator", value=self.mod)
+
+        webhook.add_embed(embed)
+        response = webhook.execute()
 
 app = Flask(__name__)
 
@@ -52,9 +88,11 @@ def viewcomment():
   if request.method == "POST":
       try:
         comment_id = int(request.form['commentid'])
-        #alert = DiscordAlert(comment_id)
-        #alert.send_alert()
-      except:
+
+        alert = DiscordAlert(comment_id)
+        alert.send_alert()
+      except Exception as e:
+        print(e)
         error = "Invalid Comment ID"
         return render_template('viewcomment.html', error=error)
   return render_template("viewcomment.html")
