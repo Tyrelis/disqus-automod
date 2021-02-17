fdd2abb5fc34f0af4c65f4e0184dce3e37839fe2
Kenny <nggkenny@gmail.com>
2021-02-17 22:12:10 +0530

    View User

--- app.py
+++ app.py
@@ -306,7 +306,7 @@ def checkcomment(comment_id):
               'username':response['response']['author']['username'],
               'content':response['response']['message'].replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>'),
               'upvotes':response['response']['likes'],
-              'downvotes':response['response']['likes'],
+              'downvotes':response['response']['dislikes'],
             }
 
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -345,7 +345,7 @@ def checkcomment(comment_id):
               'username':response['response']['author']['username'],
               'content':response['response']['message'].replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>'),
               'upvotes':response['response']['likes'],
-              'downvotes':response['response']['likes'],
+              'downvotes':response['response']['dislikes'],
             }
         
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
